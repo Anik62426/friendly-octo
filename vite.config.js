b988f39b18a90de0e1dec756e20ef90d054c489f
Anik62426 <aniketkumaratl@gmail.com>
2024-11-13 15:58:05 +0530

    fixing all issues

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    outDir: 'dist'
+  },
   server: {
     proxy: {
       "/api/": "https://raybun.onrender.com",
       "/uploads/": "https://raybun.onrender.com",
+      cors:false
     },
   },
 });
